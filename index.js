ef55cf7174c0d655f7f4a1ba88910b709e870fa7
Anjaliraghu123 <121935478+Anjaliraghu123@users.noreply.github.com>
2023-07-29 19:35:21 +0530

    index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from "./App";
+import  "./One.css";
+
+// Render the root component (App) into the DOM
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
