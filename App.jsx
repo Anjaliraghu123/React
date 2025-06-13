9831c8c52226ff91f61ef536f9663bf29b7f7c66
Anjaliraghu123 <121935478+Anjaliraghu123@users.noreply.github.com>
2025-06-13 13:46:41 +0530

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,45 +1,13 @@
-import React, { useState } from 'react';
-import Task from './Task';
-import  Index from "./Index.css";
+import Navbar from "./Components/Navbar";
 
-const App = () => {
-  const [task, setTask] = useState("");
-  const [todos, setTodos] = useState([]);
- 
-  const changeHandler = e => {
-    setTask(e.target.value);
-  };
+import "./App.css";
 
-  const submitHandler = e => {
-    e.preventDefault();
-    if (task.trim() !== "") {
-      const newTodo = { id: Date.new(), task: task };
-      setTodos([...todos, newTodo]);
-      setTask("");
-    }
-  };
 
-  const deleteHandler = indexValue => {
-    const newTodos = todos.filter((todo, index) => index !== indexValue);
-    setTodos(newTodos);
-  };
- 
-  return (
-    <div>
-      <center>
-        <div className="card">
-          <div className="card-body">
-            <h5 className="card-title">Todo Management Application</h5>
-            <form onSubmit={submitHandler}>
-              <input size="30" type="text" name="task" value={task} onChange={changeHandler} />&nbsp;&nbsp;
-              <input type='submit' value="Add" name="Add" />
-            </form>
-            <Task todos={todos} deleteHandler={deleteHandler} />
-          </div>
+export default function App(){
+    return(
+        <div className="App">
+            <Navbar/>
+           
         </div>
-      </center>
-    </div>
-  );
-};
-
-export default App;
+    )
+}
